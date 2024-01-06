9a383644c0554dab6049d20d7d1461a134c45534
KrishnaUppara <krishnamurthyu4apps@gmail.com>
2024-01-07 00:04:26 +1100

    added file add2.js

---
+++ add2.js
@@ -0,0 +1,21 @@
+
+
+let num1 = 5;
+let num2 = "10";
+let num3 = 20;
+
+let sum = num1 + num2;
+
+let sum2 = num1 + num3;
+
+let sum3 = num1 + num2 + num3;
+
+let sum4 = num1 + num3 + num2;
+
+let sum5 = num1 + num3 + num2 + num1 + num3;
+
+console.log (" the sum is : " + sum);
+console.log (" the sum2 is : " + sum2);
+console.log (" the sum3 is : " + sum3);
+console.log (" the sum4 is : " + sum4);
+console.log (" the sum5 is : " + sum5);
